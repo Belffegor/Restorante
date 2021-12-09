c55c3485697e02ff0885675bb61f8ef30ca86027
Belffegor <Belffegor2@gmail.com>
2021-12-09 03:46:38 +0300

    added state in App, forms in MenuAdmin

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,20 @@ import Order from "./Order";
 import MenuAdmin from "./MenuAdmin";
 
 class App extends React.Component {
+  state = {
+    burgers: {},
+    order: {},
+  };
+
+  addBurger = (burger) => {
+    // 1 делаем копию объекта state
+    const burgers = { ...this.state.burgers };
+    //2 в этот объект нужно добавить наш новый объект
+    burgers[`burger${Date.now()}`] = burger;
+    //3 записать наш новый объект burgers в state
+    this.setState({ burgers });
+  };
+
   render() {
     return (
       <div className="burger-paradise">
@@ -11,10 +25,10 @@ class App extends React.Component {
           <Header title="Very hot Burger" />
         </div>
         <Order />
-        <MenuAdmin />
+        <MenuAdmin addBurger={this.addBurger} />
       </div>
     );
   }
-};
+}
 
 export default App;
